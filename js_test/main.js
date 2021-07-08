4f69c5ccce7eab50cd6ffda26c7d9be6ae8e94ca
munsangu <ds-3632@hanmail.net>
2021-07-08 23:47:13 +0900

    20210708

--- js_test/main.js
+++ js_test/main.js
@@ -1,3 +1,4 @@
+import random from './getRandom'
 // 산술 연산자(arithemetic operator)
 
 console.log(1 + 2)
@@ -47,3 +48,29 @@ console.log(isEqual(2, '2'))
  console.log('||: ', x || y || z)
  console.log('!: ', !x)
 
+// 삼항 연산자(ternary operator)
+
+const t = 1 < 2
+
+if(t) {
+  console.log('참')
+}else{
+  console.log('거짓')
+}
+
+console.log(t ? '참' : '거짓')
+
+// 조건문(If statement)
+const r = random()
+
+if(r === 0){
+  console.log('r is 0')
+}else if(r === 2){
+  console.log('r is 2')
+}else if(r === 4){
+  console.log('r is 4')
+}
+else
+{
+  console.log('rest...')
+}
