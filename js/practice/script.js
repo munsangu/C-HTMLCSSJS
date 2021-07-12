c4b765638ee5b2e4131935c98e947e9b838be492
munsangu <ds-3632@hanmail.net>
2021-07-12 17:05:45 +0900

    20210712

--- js/practice/script.js
+++ js/practice/script.js
@@ -1,24 +1,28 @@
-// Simple calculator 
-
 const calculator = {
   plus: function (a, b) {
-    console.log(a + b)
+    return a + b
   },
   minus: function (a, b) {
-    console.log(a - b)
+    return a - b
   },
   multiply: function (a, b) {
-    console.log(a * b)
+    return a * b
   },
   divide: function (a, b) {
-    console.log(a / b)
+    return a / b
   },
   powOf: function (a, b) {
-    console.log(a ** b)
+    return a ** b
   }
  }
- calculator.plus(2, 3)
- calculator.minus(2, 3)
- calculator.multiply(2, 3)
- calculator.divide(3, 2)
- calculator.powOf(2, 3)
+ const plusResult = calculator.plus(2, 3)
+ const minusResult = calculator.minus(plusResult, 10)
+ const mulitiplyResult = calculator.multiply(10, minusResult)
+ const divideResult = calculator.divide(mulitiplyResult, plusResult)
+ const powOfResust = calculator.powOf(divideResult, minusResult)
+
+ console.log("더하기 : "+plusResult)
+ console.log("빼기 : "+minusResult)
+ console.log("곱하기 : "+mulitiplyResult)
+ console.log("나누기(몫) : "+divideResult)
+ console.log("제곱 : "+powOfResust)
