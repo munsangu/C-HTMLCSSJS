ee235e0682ae32da51158776d12cf15df2e8e177
munsangu <ds-3632@hanmail.net>
2021-07-12 16:29:19 +0900

    20210712

---
+++ js/practice/script.js
@@ -0,0 +1,24 @@
+// Simple calculator 
+
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b)
+  },
+  minus: function (a, b) {
+    console.log(a - b)
+  },
+  multiply: function (a, b) {
+    console.log(a * b)
+  },
+  divide: function (a, b) {
+    console.log(a / b)
+  },
+  powOf: function (a, b) {
+    console.log(a ** b)
+  }
+ }
+ calculator.plus(2, 3)
+ calculator.minus(2, 3)
+ calculator.multiply(2, 3)
+ calculator.divide(3, 2)
+ calculator.powOf(2, 3)
